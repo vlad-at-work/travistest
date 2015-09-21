3de0b011827c474a58bb9d81ae4dfe54e2430eb7
Volodymyr Golubyev <vladdypwnz@gmail.com>
2015-09-21 01:10:52 -0700

    Fixed tests

--- book.rb
+++ book.rb
@@ -1,7 +1,8 @@
 class Book
+  attr_accessor :title, :author, :category
   def initialize title, author, category
     @title = title
     @author = author
-    @category = category
+    @category = category.to_sym
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -10,4 +10,23 @@ describe Book do
       expect(@book).to be_a Book
     end
   end
+
+  describe "#new" do
+    it "Book object should have the correct title" do
+      puts @book.title + "\n\n\n"
+      expect(@book.title).to eq("Title")
+    end
+  end
+
+  describe "#new" do
+    it "Book object should have the correct Author" do
+      expect(@book.author).to eq("Author")
+    end
+  end
+
+  describe "#new" do
+    it "Book object category should be a symbol" do
+      expect(@book.category).to be_a Symbol
+    end
+  end
 end
